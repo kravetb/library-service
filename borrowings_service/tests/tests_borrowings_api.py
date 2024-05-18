584b9e5c5ddee3e55be279ff27da27b9efcd8ead
Your_Name Your_Surname <your@email>
2024-05-18 15:13:38 +0300

    Make test by borrowing list by super user and by filtering for superuser by user_id

--- borrowings_service/tests/tests_borrowings_api.py
+++ borrowings_service/tests/tests_borrowings_api.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
@@ -8,15 +9,18 @@ from borrowings_service.models import Borrowing
 
 from datetime import datetime, timedelta
 
+from book_service.tests.tests_book_api import sample_book
+from borrowings_service.serializers import BorrowingListSerializer, BorrowingDetailSerializer
+
 BORROWING_URL = reverse("borrowings-service:borrowing-list")
 
 
-def sample_bo(**params) -> Borrowing:
+def sample_borrowing(**params) -> Borrowing:
     default = {
         "borrow_date": datetime.today(),
         "expected_return_date": datetime.today() + timedelta(days=5),
-        "inventory": 5,
-        "daily_fee": 1.50,
+        "book": None,
+        "user": None,
     }
     default.update(params)
     return Borrowing.objects.create(**default)
@@ -30,3 +34,52 @@ class UnauthenticatedBorrowingsApiTest(TestCase):
     def test_auth_required(self):
         res = self.client.get(BORROWING_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class AuthenticatedBookApiTest(TestCase):
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="test@user.com",
+            password="testpassword"
+        )
+        self.test_superuser = get_user_model().objects.create_superuser(
+            email="test@seconduser.com",
+            password="secondpassword"
+        )
+        self.book = sample_book()
+        self.borrowing = sample_borrowing(book=self.book, user=self.user)
+        self.second_borrowing = sample_borrowing(book=self.book, user=self.test_superuser)
+
+    def test_borrowings_list(self):
+        self.client.force_authenticate(self.user)
+        res = self.client.get(BORROWING_URL)
+        borrowings = Borrowing.objects.filter(user=self.user.id)
+        serializer = BorrowingListSerializer(borrowings, many=True)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_borrowings_detail(self):
+        self.client.force_authenticate(self.user)
+        url = reverse("borrowings-service:borrowing-detail", args=[self.borrowing.id])
+        res = self.client.get(url)
+        borrowing = Borrowing.objects.get(id=self.borrowing.id)
+        serializer = BorrowingDetailSerializer(borrowing, many=False)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_borrowing_list_by_superuser(self):
+        self.client.force_authenticate(self.test_superuser)
+        res = self.client.get(BORROWING_URL)
+        borrowings = Borrowing.objects.all()
+        serializer = BorrowingListSerializer(borrowings, many=True)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_filtering_for_super_user_bu_user_id(self):
+        self.client.force_authenticate(self.test_superuser)
+        url = f"{BORROWING_URL}?user_id={self.user.id}"
+        res = self.client.get(url)
+        borrowings = Borrowing.objects.filter(user=self.user.id)
+        serializer = BorrowingListSerializer(borrowings, many=True)
+        self.assertEqual(res.data, serializer.data)
+
+
