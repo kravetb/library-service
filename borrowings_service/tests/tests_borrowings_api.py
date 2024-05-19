2dece822d1faa90718e35a6991639ab15d15f444
Your_Name Your_Surname <your@email>
2024-05-19 17:41:21 +0300

    Add test by return-borrowing endpoint

--- borrowings_service/tests/tests_borrowings_api.py
+++ borrowings_service/tests/tests_borrowings_api.py
@@ -5,14 +5,20 @@ from rest_framework.reverse import reverse
 
 from rest_framework.test import APIClient
 
+from book_service.models import Book
 from borrowings_service.models import Borrowing
 
 from datetime import datetime, timedelta
 
 from book_service.tests.tests_book_api import sample_book
-from borrowings_service.serializers import BorrowingListSerializer, BorrowingDetailSerializer
+from borrowings_service.serializers import (
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+    BorrowingReturnSerializer
+)
 
-BORROWING_URL = reverse("borrowings-service:borrowing-list")
+BORROWING_LIST_URL = reverse("borrowings-service:borrowing-list")
+BORROWING_DETAIL_URL = "borrowings-service:borrowing-detail"
 
 
 def sample_borrowing(**params) -> Borrowing:
@@ -33,7 +39,7 @@ class UnauthenticatedBorrowingsApiTest(TestCase):
         self.client = APIClient()
 
     def test_auth_required(self):
-        res = self.client.get(BORROWING_URL)
+        res = self.client.get(BORROWING_LIST_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
@@ -58,14 +64,14 @@ class AuthenticatedBookApiTest(TestCase):
 
     def test_borrowings_list(self):
         self.client.force_authenticate(self.user)
-        res = self.client.get(BORROWING_URL)
+        res = self.client.get(BORROWING_LIST_URL)
         borrowings = Borrowing.objects.filter(user=self.user.id)
         serializer = BorrowingListSerializer(borrowings, many=True)
         self.assertEqual(res.data, serializer.data)
 
     def test_borrowings_detail(self):
         self.client.force_authenticate(self.user)
-        url = reverse("borrowings-service:borrowing-detail", args=[self.borrowing.id])
+        url = reverse(BORROWING_DETAIL_URL, args=[self.borrowing.id])
         res = self.client.get(url)
         borrowing = Borrowing.objects.get(id=self.borrowing.id)
         serializer = BorrowingDetailSerializer(borrowing, many=False)
@@ -73,14 +79,14 @@ class AuthenticatedBookApiTest(TestCase):
 
     def test_borrowing_list_by_superuser(self):
         self.client.force_authenticate(self.test_superuser)
-        res = self.client.get(BORROWING_URL)
+        res = self.client.get(BORROWING_LIST_URL)
         borrowings = Borrowing.objects.all()
         serializer = BorrowingListSerializer(borrowings, many=True)
         self.assertEqual(res.data, serializer.data)
 
     def test_filtering_for_super_user_bu_user_id(self):
         self.client.force_authenticate(self.test_superuser)
-        url = f"{BORROWING_URL}?user_id={self.user.id}"
+        url = f"{BORROWING_LIST_URL}?user_id={self.user.id}"
         res = self.client.get(url)
         borrowings = Borrowing.objects.filter(user=self.user.id)
         serializer = BorrowingListSerializer(borrowings, many=True)
@@ -89,9 +95,24 @@ class AuthenticatedBookApiTest(TestCase):
     def test_filtering_by_is_active(self):
         self.client.force_authenticate(self.test_superuser)
         is_active = True
-        url = f"{BORROWING_URL}?is_active={is_active}"
+        url = f"{BORROWING_LIST_URL}?is_active={is_active}"
         res = self.client.get(url)
         borrowings = Borrowing.objects.filter(actual_return_date__isnull=True)
         serializer = BorrowingListSerializer(borrowings, many=True)
         self.assertEqual(res.data, serializer.data)
+
+    def test_borrowing_return_endpoint(self):
+        self.client.force_authenticate(self.user)
+        book_before = Book.objects.get(id=self.borrowing.book.id)
+        url = reverse(BORROWING_DETAIL_URL, args=[self.borrowing.id])
+        return_url = F"{url}return-borrowing/"
+        data = {
+            "actual_return_date": "2024-05-20"
+        }
+        res = self.client.patch(return_url, data=data, format="json")
+        borrowing = Borrowing.objects.get(id=self.borrowing.id)
+        book_after = Book.objects.get(id=self.borrowing.book.id)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(borrowing.actual_return_date, "2024-05-20")
+        self.assertEqual(book_before.inventory, book_after.inventory - 1)
 
