c9248b21271eb546aab0187fc9712230e43a3213
Your_Name Your_Surname <your@email>
2024-05-19 13:53:58 +0300

    Change requirements.txt file

--- library_service/settings.py
+++ library_service/settings.py
@@ -12,11 +12,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
